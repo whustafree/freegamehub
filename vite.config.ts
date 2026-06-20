bac263cf3afd0c01f86069983115fc1b7d8ced76
whustafree <magustoregustavo@gmail.com>
2026-06-20 12:15:38 -0400

    🔧 Excluir e2e/ de vitest para que no falle el CI

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test-setup.ts',
-    exclude: ['node_modules/**', 'src-backend/**'],
+    exclude: ['node_modules/**', 'src-backend/**', 'e2e/**'],
   }
 })
